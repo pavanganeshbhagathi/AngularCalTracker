fd184b11c8df84413258dcebda63c17085fa501e
pavanganeshbhagathi <pavanganeshbhagathi554@gmail.com>
2020-06-15 10:52:35 +0530

    updated

---
+++ calarioes/src/app/component/new-food/new-food.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { Food } from './../../interface/food';
+import { items } from './../../interface/foods';
+
+@Component({
+  selector: 'app-new-food',
+  templateUrl: './new-food.component.html',
+  styleUrls: ['./new-food.component.css']
+})
+export class NewFoodComponent implements OnInit {
+
+  items: Food[];
+  newFoodName = 'Enter Food Name';
+  newFoodCalories = 0;
+  newFoodImage = '';
+
+  constructor() {
+   }
+
+  ngOnInit(): void {
+    this.items = items;
+  }
+
+  addFood()
+  {
+    const newFood = {
+      name: this.newFoodName,
+      calories: this.newFoodCalories,
+      image: this.newFoodImage,
+      count: 0
+    };
+
+    this.items.unshift(newFood);
+  }
+
+  addCount(count, item): any{
+    item.count += count;
+  }
+
+}
